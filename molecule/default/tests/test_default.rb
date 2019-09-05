57c8ab5a9e4a9073815b03ae5f2627547b7e84b8
John Chen <johnchen456@gmail.com>
2019-09-05 15:28:16 -0400

    test: added tests to check mount status

--- molecule/default/tests/test_default.rb
+++ molecule/default/tests/test_default.rb
@@ -1,1 +1,23 @@
 # frozen_string_literal: true
+
+describe mount('/efs') do
+  it { should be_mounted }
+  its('device') { should eq 'overlay' }
+  its('type') { should eq 'overlay' }
+end
+
+describe file('/efs/test') do
+  it { should exist }
+end
+
+describe command('ls /efs/*/info') do
+  its('exit_status') { should eq 0 }
+end
+
+describe command('ls /efs/*/info/distro_name') do
+  its('exit_status') { should eq 0 }
+end
+
+describe command('ls /efs/*/scons-cache') do
+  its('exit_status') { should eq 0 }
+end
